abc6aad977c22a2954bad35bb979c0017758df26
dongxs <dongxs@hffss.com>
2017-12-05 16:05:40 +0800

    更新登录页面

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,7 +3,11 @@ const snowf = require('snowframework');
 const homeController = {
     indexAction: function (req, res) {
         res.send(snowf.actionResult.view, null);
+    },
+    loginAction: function(req,res){
+        res.send(snowf.actionResult.view);
     }
+
 }
 
 module.exports = homeController;
